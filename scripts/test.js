0fd35b9714dfc1c51b288a33a6203d06f81ab932
vilas-gannaram <vilasgannaram@gmail.com>
2026-08-02 00:48:44 +0530

    Update test script with new shortkeys and adjust load test parameters

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,41 @@
-import http from 'k6/http';
 import { check } from 'k6';
+import http from 'k6/http';
 
 const shortkeys = [
-	'36kawpps',
-	'n9fnxs47',
-	'9w8tn8yx',
-	'mswzdt9t',
-	'4qjhgvfk',
-	'e9g3yb4g',
+	'6y2c6t79',
+	'cn2vd59v',
+	'mdxtdrk2',
+	'd2z4tzgb',
+	'j8cy6efy',
+	'zyd3gzh3',
+	'ej9j5cq7',
+	'4bd3bfxv',
+	'qtwgejgm',
+	'ba3za6jy',
+	'trw8qhcc',
+	'ytdqmavk',
+	'taxxk3qn',
+	'2eds9e3b',
+	'8v6dtxtw',
+	'8q6qfknb',
+	'm68xr92t',
+	'9sygtyck',
+	'xmyhfjnp',
+	'tamgvg26',
+	'kaka5s96',
+	'545jtw7d',
 ];
 
 export const options = {
-	vus: 100, // 10 virtual users running simultaneously
-	iterations: 10000, // Total number of requests across all VUs
+	vus: 10, // 10 virtual users running simultaneously
+	iterations: 100, // Total number of requests across all VUs
 };
 
 export default function () {
 	// Pick a random key from the list
 	const key = shortkeys[Math.floor(Math.random() * shortkeys.length)];
 
-	let res = http.get(`http://localhost:8080/${key}`);
+	let res = http.get(`https://lynkr-tor7.onrender.com/${key}`);
 
 	check(res, {
 		'final status is 200': (r) => r.status === 200,
